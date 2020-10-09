59aab282d9b5c9c091f17519ccad1abb04109363
Peter Nigh <peternigh@Peters-MacBook-Pro.local>
2020-10-09 10:58:36 -0500

    initial data render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,26 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [character, getCharacter] = useState();
+
+  useEffect(()=>{
+    axios.get('https://rickandmortyapi.com/api/character/[1,2,3,4,5]')
+    .then(res=>{
+      getCharacter(res.data)
+      console.log(res)
+    })
+    .catch(err=>{
+
+    })
+
+  } , [])
+
+
+  if (!character) return <div><h1>Loading ...</h1></div>
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character character={character} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,36 @@
 // Write your Character component here
+import React, {useState} from 'react'
+
+const Character = (props) => {
+    const character = props
+
+
+    return (
+        <div>
+            {
+                character.character.map(character =>{
+                    return ( 
+                        <div>
+                            <div>
+                                <h2>Name: {character.name}</h2>
+                        
+                            </div>
+                            {/* <div>
+                                <P>Gender: {character.gender != 'n/a' ? character.gender : 'unkown'}</P>
+                                <P>Birth year: {character.birth_year}</P>
+                            </div> */}
+                        </div>
+                            
+                    );
+                })
+            }
+    
+    
+        </div>
+    
+    )
+    
+}
+
+
+export default Character
